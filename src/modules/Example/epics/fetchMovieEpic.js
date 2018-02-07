4a86ac0be503a737028506a30656d4f87a0c0707
Quentin Hausser <hausser.quentin@gmail.com>
2018-02-07 10:23:28 +0100

    More example use case

--- src/modules/Example/epics/fetchMovieEpic.js
+++ src/modules/Example/epics/fetchMovieEpic.js
@@ -31,7 +31,18 @@ const fetchMovieEpic = (action$, store) =>
         .catch(error => Observable.of(fetchMovieRejected(error, action.extras)))
         .takeUntil(action$.ofType(FETCH_MOVIE_REQUEST)));
 
+// Example of a filter in an epic
+const fetchMovieEpicTest = (action$, store) =>
+  action$.ofType(FETCH_MOVIE_REQUEST)
+    .filter(action => (
+      action.payload.movieID === '5fdfb320-2a02-49a7-94ff-5ca418cae602'
+    ))
+    .mergeMap(action =>
+      Observable.of({ type: 'WHEN_MARNIE_WAS_THERE', payload: action }));
+
+
 export default combineEpics(
+  fetchMovieEpicTest,
   fetchMovieEpic,
   testEpic,
 );
